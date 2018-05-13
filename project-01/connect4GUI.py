2cabbb64c3f25822cb4fe7d25ca1e523b0ad31d0
BorisMonth <1309272+BorisMonth@users.noreply.github.com>
2018-05-13 13:04:44 +0200

    started work on the gui

--- project-01/connect4GUI.py
+++ project-01/connect4GUI.py
@@ -6,3 +6,39 @@ Created on Fri May 11 19:35:15 2018
 @author: Daniel Biskup
 """
 
+import sys, pygame
+import time
+pygame.init()
+
+size = width, height = 320, 240
+size = width, height = 1366, 768
+# fix window size
+
+speed = [2, 2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+#ball = pygame.image.load("ball.gif")
+#ballrect = ball.get_rect()
+
+running = True
+while running:
+    #events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+
+ #   ballrect = ballrect.move(speed)
+#    if ballrect.left < 0 or ballrect.right > width:
+#        speed[0] = -speed[0]
+#    if ballrect.top < 0 or ballrect.bottom > height:
+        #speed[1] = -speed[1]
+
+    screen.fill(black)
+#    screen.blit(ball, ballrect)
+    pygame.display.flip()
+ 
+pygame.quit()
+            
