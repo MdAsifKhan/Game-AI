4c37f60d4cbdcbcd820e7089e3b5587b9d876558
BorisMonth <1309272+BorisMonth@users.noreply.github.com>
2018-07-09 14:50:44 +0200

    Added experiments, ran them, and deleted older results.

--- project-03/Connect4/Experiments.py
+++ project-03/Connect4/Experiments.py
@@ -6,25 +6,27 @@ Created on Sun Jul  8 16:44:49 2018
 @author: boris
 """
 
-import connect4class
-
 #%%
+import connect4class
 # 3 vs Random
 connect4class.run_experiment(experiment_name='3vs0',
-               polt_every_nth=100,
+               polt_every_nth=1,
                num_games = 10000,
                players_to_use_MinMax = [1],
                max_depth=[3],
                board_size = [19,19])
 #%%
+import connect4class
 # 3vs1
 connect4class.run_experiment(experiment_name='3vs1',
-               polt_every_nth=2,
+               polt_every_nth=1,
                num_games = 10000,
                players_to_use_MinMax = [1,-1],
                max_depth=[3,1],
                board_size = [19,19])
 #%%
+import connect4class
+# CANCLED!
 # 3vs2
 connect4class.run_experiment(experiment_name='3vs2',
                polt_every_nth=1,
@@ -33,10 +35,11 @@ connect4class.run_experiment(experiment_name='3vs2',
                max_depth=[3,2],
                board_size = [19,19])
 #%%
+import connect4class
 # 3vs3
 connect4class.run_experiment(experiment_name='3vs3',
-               polt_every_nth=2,
-               num_games = 10,
+               polt_every_nth=1,
+               num_games = 10000,
                players_to_use_MinMax = [1,-1],
                max_depth=[3,3],
                board_size = [19,19])
